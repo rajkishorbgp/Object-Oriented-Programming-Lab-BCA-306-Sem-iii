7a479c2d051350623514f44a3d0a7a781f19c3e5
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-08 14:49:31 +0530

    Write a C++ Program to swap Two Numbers without using third variable.

---
+++ 1_swapTwoNumberUsingThirdVariable.cpp
@@ -0,0 +1,37 @@
+/*
+                    @program : 1.
+        Write a C++ Program to swap Two Numbers without using third variable.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter the value a= ";
+    cin >> a;
+    cout << "Enter the value b= ";
+    cin >> b;
+    cout << "Befour swap the value " << endl;
+    cout << "a= " << a << endl
+         << "b= " << b << endl;
+    c = a;
+    a = b;
+    b = c;
+    cout << "After swap the value " << endl;
+    cout << "a= " << a << endl
+         << "b= " << b << endl;
+}
+
+/*
+Output:
+Enter the value a= 5
+Enter the value b= 8
+Befour swap the value
+a= 5
+b= 8
+After swap the value
+a= 8
+b= 5
+*/
