81cca983f781c1bacc4873b55a2cde3e1c21910d
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-09 15:22:03 +0530

    Write a C++ Program to display Student Details using class.

---
+++ 7_display_Student_Details_usin_class.cpp
@@ -0,0 +1,44 @@
+/*
+               @program: 7
+    Write a C++ Program to display Student Details using class.
+*/
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    long long int rollNo;
+    string course;
+    int marks;
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Roll no: " << rollNo << endl;
+        cout << "Course: " << course << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1.name = "Raj Kishor";
+    s1.rollNo = 210000106095;
+    s1.course = "BCA";
+    s1.marks = 400;
+
+    s1.display();
+
+    return 0;
+}
+
+/*
+    Output:
+    Name: Raj Kishor
+    Roll no: 210000106095
+    Course: BCA
+    Marks: 400
+*/
