98628d2e546e2ed4ba24e7e9eeb1714490d0bb36
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-13 13:33:33 +0530

    Write a C++ Program to swap Two Numbers without using third variable.

---
+++ 14_convert_string_into_upper_or_lower.cpp
@@ -0,0 +1,36 @@
+/*
+                            peogram: 14
+    Write a program in C++ to convert a string of characters into upper or lower case.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    int i = 0;
+    cout << "Enter the string: ";
+    cin >> str;
+
+    while (str[i])
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = tolower(str[i]);
+        }
+        else
+        {
+            str[i] = toupper(str[i]);
+        }
+        i++;
+    }
+    cout << "convert a string: " << str;
+
+    return 0;
+}
+
+/*
+    Output:
+    Enter the string: RajKishor
+    convert a string: rAJkISHOR
+*/
