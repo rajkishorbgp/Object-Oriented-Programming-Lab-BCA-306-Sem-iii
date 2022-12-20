fa835e1e7362a23893345078956608de5f692286
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-20 11:35:21 +0530

    texting

---
+++ 18_areaOfRectangleUsingFunctionOverloading.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int areaOfRectangle(int l, int w)
+{
+    return l * w;
+}
+float areaOfRectangle(float l, float w)
+{
+    return l * w;
+}
+
+int main()
+{
+    int l, w;
+    cout << "Enter the length(L) = ";
+    cin >> l;
+    cout << "Enter the Width(W) = ";
+    cin >> w;
+
+    cout << "Area of rectangle: " << areaOfRectangle(l, w);
+
+    return 0;
+}
+
+/*
+    Output:
+    Enter the length(L) = 4
+    Enter the Width(W) = 8
+    Area of rectangle: 32
+*/
